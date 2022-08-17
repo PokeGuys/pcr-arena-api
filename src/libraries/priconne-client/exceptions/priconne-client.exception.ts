b5f0a4873b241151057515c39e395374826c67a7
PokeGuys <5060799+PokeGuys@users.noreply.github.com>
2022-08-17 15:30:38 +0800

    fix(profile): mutex lock never release when exception happened

--- src/libraries/priconne-client/exceptions/priconne-client.exception.ts
+++ src/libraries/priconne-client/exceptions/priconne-client.exception.ts
@@ -2,8 +2,14 @@ import { ApiException } from '@common/exceptions';
 import { PRICONNE_API_ERROR } from '@libraries/priconne-client/priconne.constants';
 
 export class PriconneClientException extends ApiException {
-  constructor(message: string, status: number, resultCode: number) {
+  public readonly errorStatus: number;
+
+  public readonly errorResultCode?: any;
+
+  constructor(message: string, status: number, resultCode?: any) {
     super({ code: PRICONNE_API_ERROR, payload: { status, resultCode } });
     this.message = message;
+    this.errorStatus = status;
+    this.errorResultCode = resultCode;
   }
 }
--- src/modules/player-profile/player-profile.service.ts
+++ src/modules/player-profile/player-profile.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { EntityNotFoundException } from '@common/exceptions';
+import { PriconneClientException } from '@libraries/priconne-client/exceptions';
 import { PriconneClient } from '@libraries/priconne-client/priconne.client';
 import { AccountPoolService } from '@modules/account/pool';
 import { RetrieveProfileDto, PlayerProfileDto } from './dtos';
@@ -18,32 +20,41 @@ export class PlayerProfileService {
     // Lock the account so that other requests can't use it
     this.logger.log(`Acquiring lock for account ${account.viewerId} (server ${account.server})`);
     const releaser = await this.pool.acquire(account.server, account.viewerId);
-    const { data: profile, data_headers: headers } = await this.priconneClient.getProfile(
-      dto.id,
-      account,
-    );
-    this.logger.log(`Releasing lock for account ${account.viewerId} (server ${account.server})`);
-    releaser();
-    this.logger.debug(profile, `Retrieved profile ${dto.id} from server ${dto.server}`);
-    return {
-      viewerId: dto.id,
-      server: dto.server,
-      name: profile.user_info.user_name,
-      clanName: profile.clan_name || undefined,
-      level: profile.user_info.team_level,
-      totalPower: profile.user_info.total_power,
-      lastLoginAt: profile.last_login_at,
-      serverTime: headers.servertime,
-      arena: {
-        group: profile.user_info.arena_group,
-        rank: profile.user_info.arena_rank,
-        createdAt: profile.user_info.arena_time,
-      },
-      grandArena: {
-        group: profile.user_info.grand_arena_group,
-        rank: profile.user_info.grand_arena_rank,
-        createdAt: profile.user_info.grand_arena_time,
-      },
-    };
+    try {
+      const { data: profile, data_headers: headers } = await this.priconneClient.getProfile(
+        dto.id,
+        account,
+      );
+      this.logger.log(`Releasing lock for account ${account.viewerId} (server ${account.server})`);
+      this.logger.debug(profile, `Retrieved profile ${dto.id} from server ${dto.server}`);
+      return {
+        viewerId: dto.id,
+        server: dto.server,
+        name: profile.user_info.user_name,
+        clanName: profile.clan_name || undefined,
+        level: profile.user_info.team_level,
+        totalPower: profile.user_info.total_power,
+        lastLoginAt: profile.last_login_at,
+        serverTime: headers.servertime,
+        arena: {
+          group: profile.user_info.arena_group,
+          rank: profile.user_info.arena_rank,
+          createdAt: profile.user_info.arena_time,
+        },
+        grandArena: {
+          group: profile.user_info.grand_arena_group,
+          rank: profile.user_info.grand_arena_rank,
+          createdAt: profile.user_info.grand_arena_time,
+        },
+      };
+    } catch (e) {
+      if (e instanceof PriconneClientException && e.errorStatus === 6) {
+        throw new EntityNotFoundException(PlayerProfileDto.name);
+      }
+      throw e;
+    } finally {
+      this.logger.log(`Releasing lock for account ${account.viewerId} (server ${account.server})`);
+      releaser();
+    }
   }
 }
